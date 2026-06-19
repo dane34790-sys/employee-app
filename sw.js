e2840beb515a7e0c60769159c8fc245e136fbc95
dane34790-sys <dane34790@gmail.com>
2026-06-19 09:04:30 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,49 @@
-{
-  "name": "Employee App",
-  "short_name": "Employee",
-  "start_url": "./index.html",
-  "scope": "./",
-  "display": "standalone",
-  "background_color": "#ffffff",
-  "theme_color": "#000000",
+const CACHE_NAME = "employee-app-v1";
 
-  "icons": [
-    {
-      "src": "images/app-icon.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "images/app-icon.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
-}
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+
+  "./images/app-icon.png",
+  "./images/employee-bg.png",
+  "./images/login-bg.png",
+  "./images/mypdf.jpg",
+  "./images/telegram.png",
+  "./images/trustwallet.png",
+  "./images/line.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
