3c15095469ef1fda25b2cda96530123850fe1c3f
dane34790-sys <dane34790@gmail.com>
2026-06-20 02:53:59 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "employee-app-v2";
+const CACHE_NAME = "employee-app-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -8,32 +8,33 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-// نصب و کش کردن فایل‌ها
+// نصب
 self.addEventListener("install", event => {
+  self.skipWaiting(); // فعال شدن سریع
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-
-  // فعال شدن فوری نسخه جدید
-  self.skipWaiting();
 });
 
-// حذف کش‌های قدیمی
+// فعال‌سازی
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
       );
     }).then(() => self.clients.claim())
   );
 });
 
-// اول از کش، اگر نبود از اینترنت
+// fetch
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
