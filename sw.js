6561b0be108cf5292096cc4d1b9cc95264b3bb23
dane34790-sys <dane34790@gmail.com>
2026-06-19 08:58:33 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,11 +5,22 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+
+  // 📌 مهم: آیکن و عکس‌ها هم اضافه شد
+  "./images/app-icon.png",
+  "./images/employee-bg.png",
+  "./images/login-bg.png",
+  "./images/mypdf.jpg",
+  "./images/telegram.png",
+  "./images/trustwallet.png",
+  "./images/line.png"
 ];
 
 // نصب و کش کردن فایل‌ها
 self.addEventListener("install", event => {
+  self.skipWaiting(); // 🚀 سریع فعال شود
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -22,11 +33,14 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       );
     })
   );
+
+  self.clients.claim(); // 🚀 کنترل فوری
 });
 
 // گرفتن فایل‌ها از کش یا اینترنت
