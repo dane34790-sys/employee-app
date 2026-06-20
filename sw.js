362494363f31cae996275e8afc17d9893bfe8ae8
dane34790-sys <dane34790@gmail.com>
2026-06-20 15:20:49 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
-const CACHE_NAME = "employee-app-v3";
+const CACHE_NAME = "employee-app-v5";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./images/login-bg.png",
+  "./images/employee-bg.png"
 ];
 
 // نصب
 self.addEventListener("install", event => {
-  self.skipWaiting(); // فعال شدن سریع
+  self.skipWaiting(); // فعال شدن فوری
 
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -24,17 +26,15 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       );
     }).then(() => self.clients.claim())
   );
 });
 
-// fetch
+// fetch (نسخه امن‌تر)
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
