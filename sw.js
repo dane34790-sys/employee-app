b8db8801004b8af1144fdc406f9c4bfbdb35b24e
dane34790-sys <dane34790@gmail.com>
2026-06-20 16:46:59 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,87 @@
-const CACHE_NAME = "employee-app-v6";
+const CACHE_NAME = "employee-app-v7";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./images/app-icon.png",
+  "./images/login-bg.png",
+  "./images/employee-bg.png"
 ];
 
+// نصب
 self.addEventListener("install", event => {
+
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   );
+
 });
 
+// فعال‌سازی
 self.addEventListener("activate", event => {
+
   event.waitUntil(
+
     caches.keys().then(keys =>
+
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
       )
+
     ).then(() => self.clients.claim())
+
   );
+
 });
 
+// دریافت فایل‌ها
 self.addEventListener("fetch", event => {
+
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then(r => r || fetch(event.request))
+
+    caches.match(event.request).then(cached => {
+
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request)
+        .then(response => {
+
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
+            return response;
+          }
+
+          const responseClone = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+
+          return response;
+
+        })
+        .catch(() => caches.match("./index.html"));
+
+    })
+
   );
+
 });
