d07b0833ca3e3958e251aba7d994caccb2d0f0fd
dane34790-sys <dane34790@gmail.com>
2026-06-21 12:32:39 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,30 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, {
   polling: true,
 });
 
+const ADMIN_ID = 8494308052;
+
 bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text || "";
+
+  console.log("MESSAGE:", chatId, text);
+
+  // پیام ادمین
+  if (chatId === ADMIN_ID) {
+    return;
+  }
+
+  // پیام کارمند → ادمین
   await bot.sendMessage(
-    msg.chat.id,
-    "✅ پیام شما دریافت شد:\n\n" + (msg.text || "")
+    ADMIN_ID,
+    `📩 پیام جدید\n\n${text}\n\nID:${chatId}`
+  );
+
+  // تایید به کارمند
+  await bot.sendMessage(
+    chatId,
+    "✅ پیام شما برای مدیریت ارسال شد."
   );
 });
 
-console.log("Bot is running...");
+console.log("🤖 Bot is running...");
