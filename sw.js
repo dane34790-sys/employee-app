81f35c9cb2ad073580db623e2d520802a3551ecf
dane34790-sys <dane34790@gmail.com>
2026-06-24 18:32:10 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,52 @@
-const CACHE_NAME = "employee-app-v10";
+const CACHE_NAME = "employee-app-v11";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
+  "/employee-app/",
+  "/employee-app/index.html",
+  "/employee-app/style.css",
+  "/employee-app/app.js",
+  "/employee-app/manifest.json"
 ];
 
 self.addEventListener("install", event => {
-
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
-
 });
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
+          if (key !== CACHE_NAME) return caches.delete(key);
         })
       )
     ).then(() => self.clients.claim())
   );
-
 });
 
 self.addEventListener("fetch", event => {
-
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-
     fetch(event.request)
       .then(response => {
-
         const clone = response.clone();
 
-        caches.open(CACHE_NAME)
-          .then(cache => cache.put(event.request, clone));
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
 
         return response;
-
       })
-      .catch(() =>
-        caches.match(event.request)
-      )
-
+      .catch(() => {
+        return caches.match(event.request)
+          .then(res => {
+            return res || caches.match("/employee-app/index.html");
+          });
+      })
   );
-
 });
